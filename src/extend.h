471e9333ee0665523eac86d426b2f0532b24e29c
Gustavo Toledo <gustavot53@gmail.com>
2016-12-01 14:16:24 -0200

    Updated header file

--- src/extend.h
+++ src/extend.h
@@ -5,10 +5,10 @@
 //  Version: 1.00.0
 //
 // Define global variables //
-//
-//
-//
-// 
+int table[3][3]; // Set global syntax //
+int player=0x00;
+int control=0x00;
+
 //
 // Here, list all functions that going to be used. //
 //
@@ -16,6 +16,5 @@ void clean_table();
 void print_table();
 int check_winner();
 int make_move(int player);
-
 
 // End of header //
